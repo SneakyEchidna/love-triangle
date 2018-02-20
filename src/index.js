d8d8ff99baab62ce7cc2d6baba36f671ffea0213
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-02-20 17:09:10 +0300

    all tests passed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let triangles = 0;
+  let ignored = [];
+  for (let i = 0; i < preferences.length; i++) {
+    let firstIndex = i;
+    let secondIndex = preferences[i] - 1;
+    let thirdIndex = preferences[secondIndex] - 1;
+    let backfire = preferences[thirdIndex] - 1;
+
+    let first = preferences[thirdIndex];
+    let second = preferences[firstIndex];
+    let third = preferences[secondIndex];
+    if (
+      ignored.includes(first) ||
+      ignored.includes(second) ||
+      ignored.includes(third)
+    ) {
+      continue;
+    }
+    if (firstIndex === backfire && (first != second || second != third)) {
+      triangles++;
+      ignored.push(...[first, second, third]);
+      // console.log(ignored);
+    }
+  }
+  return triangles;
 };
